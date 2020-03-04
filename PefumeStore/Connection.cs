9cebe4a2396de62a2806fd76eba6ab59547d4142
hieuthanh19 <kieuhieuthanh2@gmail.com>
2020-03-04 21:49:52 +0700

    add object + connection string

---
+++ PefumeStore/Connection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;  //add from references
+
+namespace PefumeStore
+{
+    class Connection
+    {
+        public SqlConnection sqlConnection()
+        {
+            return new SqlConnection(ConfigurationManager.ConnectionStrings["PefumeStore.Properties.Settings.Perfume_StoreConnectionString"].ConnectionString);  
+            //see connection string in app.config in solution tree
+        }
+    }
+}
